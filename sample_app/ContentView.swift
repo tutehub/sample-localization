3ebb948ab9bfde5359b4efeb3367acaf0d413b9d
Trisha <chandrasekar.trisha@gmail.com>
2023-06-11 13:25:59 +1000

    Initial Commit

---
+++ sample_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sample_app
+//
+//  Created by Trisha Chandrasekar on 11/6/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sample_app/sample_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_appApp.swift
+//  sample_app
+//
+//  Created by Trisha Chandrasekar on 11/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct sample_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
